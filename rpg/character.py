88aca1bb0973beb40c2842ad07183c11b36ffdce
MrEll3n <frogomanzc@gmail.com>
2023-11-22 10:03:04 +0100

    update rpg

--- rpg/character.py
+++ rpg/character.py
@@ -4,11 +4,23 @@ class Character:
         self.stregth = strength
         self.agility = agility
         self.vitality = vitality
-        self.HAND_LEFT:int = 0
-        self.HAND_RIGHT:int = 0
+        self.HAND_LEFT:int = 2
+        self.HAND_RIGHT:int = 1
     
     def attack(self):
         return self.strength
     
     def defend(self, attack:int) -> int:
         pass
+
+    def is_alive(self) -> bool:
+        if self.vitality > 0:
+            return True
+        else:
+            return False
+    
+    def take_weapon(self, weapon:Weapon|None, hand:int) -> bool:
+        pass
+
+    def __str__(self) -> str:
+        return f"{self.name} [{self.vitality}] ({}/{})"
